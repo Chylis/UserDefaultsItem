38ed212e01bc5a3c7f242b325684035fb3df6652
Magnus Eriksson <chylis88@gmail.com>
2020-02-08 13:33:39 +0100

    UserDefaultItem property wrapper

--- Sources/UserDefaultsItem/UserDefaultsItem.swift
+++ Sources/UserDefaultsItem/UserDefaultsItem.swift
@@ -1,3 +1,24 @@
-struct UserDefaultsItem {
-    var text = "Hello, World!"
+import Foundation
+
+@propertyWrapper
+struct UserDefaultsItem<T> {
+    private let key: String
+    private let defaultValue: T
+    private let storage: UserDefaults
+    
+    init(key: String, defaultValue: T, storage: UserDefaults = .standard) {
+        self.key = key
+        self.defaultValue = defaultValue
+        self.storage = storage
+    }
+    
+    public var wrappedValue: T {
+        get {
+            storage.object(forKey: key) as? T ?? defaultValue
+        }
+        set {
+            storage.setValue(newValue, forKey: key)
+            storage.synchronize()
+        }
+    }
 }
--- Tests/UserDefaultsItemTests/UserDefaultsItemTests.swift
+++ Tests/UserDefaultsItemTests/UserDefaultsItemTests.swift
@@ -1,15 +1,34 @@
 import XCTest
 @testable import UserDefaultsItem
 
-final class UserDefaultsItemTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(UserDefaultsItem().text, "Hello, World!")
-    }
+private let name1 = "defaultName"
+private let name2 = "new name"
 
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+struct UserSettings {
+    @UserDefaultsItem(key: "username", defaultValue: name1)
+    var username: String
+}
+
+// The tests are executed in alphabetical order, thus the naming test1, test2, test3, etc...
+final class UserDefaultsItemTests: XCTestCase {
+    override class func setUp() {
+        super.setUp()
+        var settings = UserSettings()
+        settings.username = name1
+    }
+    
+    func test1_DefaultValues() {
+        let settings = UserSettings()
+        XCTAssertEqual(settings.username, name1)
+    }
+    
+    func test2_UpdateValues() {
+        var settings = UserSettings()
+        settings.username = name2
+        XCTAssertEqual(settings.username, name2)
+    }
+    
+    func test3_PersistedValues() {
+        XCTAssertEqual(UserSettings().username, name2)
+    }
 }
